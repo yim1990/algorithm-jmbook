59d4196005b37f4b20786dab67d2a8402ec98b21
SeungjinIm <natureyim@gmail.com>
2014-04-20 21:59:32 +0900

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
 
 public class Main {
+	public static int cache[][]=new int[30][30]; 
+	public static int bino(int n, int r) {
+		if(r==0 || n==r) {
+			return 1;
+		}
+		if(cache[n][r] != 0) {
+			return cache[n][r];
+		}
+		cache[n][r]=bino(n-1, r-1) + bino(n-1, r);
+		return cache[n][r];
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(bino(25,12));
 
 	}
 }
